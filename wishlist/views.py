c55ab99c60a7be620f3364b93d28933e8cecf68c
ariefbraja <marief.braja@gmail.com>
2022-09-14 23:42:10 +0700

    Lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
 # Create your views here.
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
@@ -10,3 +12,20 @@ def show_wishlist(request):
     'nama': 'Muhammad Arief Braja Putra'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request,id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
